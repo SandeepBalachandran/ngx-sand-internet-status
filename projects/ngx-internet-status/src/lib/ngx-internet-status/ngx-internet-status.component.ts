2657c9df14f116cfa3574933dad5d178776ea627
Sandeep B <sandeepb@reflections.local>
2021-01-15 21:33:43 +0530

    settings added

--- projects/ngx-internet-status/src/lib/ngx-internet-status/ngx-internet-status.component.ts
+++ projects/ngx-internet-status/src/lib/ngx-internet-status/ngx-internet-status.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { NgxInternetStatusService,  } from "../service/ngx-internet-status.service";
 import { OnlineStatusType } from './type.enum'
+import { Settings } from '../models/settings.model'
 
 @Component({
   selector: "ngx-sand-internet-status",
@@ -25,9 +26,39 @@ export class NgxInternetStatusComponent implements OnInit,OnChanges {
   }
 
   ngOnInit(): void {
+    this.settingsInit();
   }
   ngOnChanges(): void {
 
   }
 
+  /**
+   * initialize the settings and other properties
+   */
+  private settingsInit(): void {
+    const settings: Settings = {
+      type :'banner',
+      onlineTitle:'Title - Online',
+      offlineTitle:'Title - offline',
+      onlineText:'online',
+      offlineText:'offline',
+    };
+    /*
+      * if incoming settings object is empty ,
+      * it will be initialized with predefiened values.
+    */
+    if (this.settings === 'undefined' || Object.keys(this.settings).length === 0) {
+      this.settings = { ...settings };
+    }
+
+    /*
+      * Replacing predifined values by incoming values.
+      * If no incoming value, predefined value will be taken
+    */
+
+
+    for (const key of Object.keys(settings)) {
+      this.settings[key] = this.settings[key] ? this.settings[key] : settings[key];
+    }
+  }
 }
